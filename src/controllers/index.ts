3ca62d768acd6fb1e3877e3ebe7b1b91da8caa6f
Mauricio Roa <mauricioroa@iMac-de-Mauricio.local>
2021-11-13 13:28:32 -0500

    Creacion sprint 1 master team

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './empresa-empleado.controller';
+export * from './empleado-empresa.controller';
+export * from './empresa.controller';
+export * from './empleado.controller';
